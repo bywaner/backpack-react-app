b6bf0d9cc205baf133b925648f52811b945c6409
waner-research <110174120+bywaner@users.noreply.github.com>
2025-07-02 18:48:07 +1000

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import BpkText from '@skyscanner/backpack-web/bpk-component-text';
+import BpkButton from '@skyscanner/backpack-web/bpk-component-button';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <BpkText tagName="h1" textStyle="xl">
+        🎒 Hello from Backpack React App!
+      </BpkText>
+      <BpkButton onClick={() => alert('Hello Backpack!')}>
+        Click Me
+      </BpkButton>
+    </div>
+  );
+}
+
+export default App;
